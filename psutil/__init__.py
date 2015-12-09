c1c06f55754e232cc50225e7c0c18533d28a2833
Arnon Yaari <arnony@infinidat.com>
2015-12-09 11:37:00 +0200

    bump version

--- psutil/__init__.py
+++ psutil/__init__.py
@@ -163,7 +163,7 @@ __all__ = [
 ]
 __all__.extend(_psplatform.__extra__all__)
 __author__ = "Giampaolo Rodola'"
-__version__ = "3.3.1"
+__version__ = "3.3.0.1"
 version_info = tuple([int(num) for num in __version__.split('.')])
 AF_LINK = _psplatform.AF_LINK
 _TOTAL_PHYMEM = None
